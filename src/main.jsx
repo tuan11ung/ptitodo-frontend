3bccd39f6aca54e712766f77d95ea6b2e67103ce
Tuan11ung <nguyenduytuan11ung@gmail.com>
2025-11-03 16:41:09 +0700

    add css baseline

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import CssBaseline from '@mui/material/CssBaseline';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -5,6 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <CssBaseline />
     <App />
   </StrictMode>,
 )
