6af51c6ae267a1b4c9c8ab42ee3a5ca2a7bdf8b5
Tuan11ung <nguyenduytuan11ung@gmail.com>
2025-11-28 13:54:28 +0700

    add placeholder card functionality for empty columns in drag-and-drop

--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
@@ -36,7 +36,9 @@ function Card({ card }) {
       sx={{
         cursor: 'pointer',
         boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
-        overflow: 'unset'
+        overflow: 'unset',
+        opacity: card.FE_PlaceholderCard ? '0' : '1',
+        position: card.FE_PlaceholderCard ? 'fixed' : 'unset'
       }}
     >
       {card?.cover && <CardMedia sx={{ height: 140 }} image={card?.cover} /> }
